c2340b6e0dd1ac1cab502e3c20bdf30e8112ace4
Ana Sequera <147461093+AnaSequera@users.noreply.github.com>
2025-05-05 19:59:43 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -3,9 +3,25 @@ import "./style.css";
 
 
 import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/4geeks.ico"; 
 
+let who = ['Alejandra', 'Ana', 'David', 'Juan']
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car','my kitchen'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function getRamdonNumber(a,b){
+  return Math.floor(Math.random() * (b - a) + a);
+}
+
+function getRamdon(anyArray){
+  let max = anyArray.length - 1;
+  let min = 0;
+  let random = getRamdonNumber (min, max);
+  return anyArray[random];
+}
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+ document.querySelector('#excuse').innerHTML = getRamdon(who) + " " + getRamdon(action) + " " + getRamdon(what) + " "+ getRamdon(when);
+
 };
---
+++ src/when.js
@@ -0,0 +1,1 @@
+export let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
